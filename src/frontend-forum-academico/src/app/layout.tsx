43f28f1016513c38b00c4d77810b8fe90817cc8a
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-08-06 20:34:49 -0300

    Criando componente de toast para mostrar conteudo em tela

--- src/frontend-forum-academico/src/app/layout.tsx
+++ src/frontend-forum-academico/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import CustomToastContainer from "@/components/ToastContainer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +27,8 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <AuthProvider>
-          {children}
+          <CustomToastContainer />
+            {children}
         </AuthProvider>
       </body>
     </html>
