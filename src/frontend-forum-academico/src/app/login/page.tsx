af4004a3d1fe6eea38456d67d8c89630adf39edb
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-08-06 19:12:05 -0300

    Ajustes para criar a tela de login

--- src/frontend-forum-academico/src/app/login/page.tsx
+++ src/frontend-forum-academico/src/app/login/page.tsx
@@ -1,5 +1,32 @@
-export default function Login(){
-    return<>
-        <h1>Login page aqui</h1>
-    </>
+import "./login_style.css";
+
+
+export default function Login() {
+    return (
+        <>
+            <div id="logo-div">
+                <img src="/logo.svg" alt="logo" />
+            </div>
+            <div id="login-div">
+                <h1>Compartilhe Conhecimento</h1>
+                <form id="login-info">
+                    <div className="form-div">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" />
+                    </div>
+
+                    <div className="form-div">
+                        <label htmlFor="pass">Password</label>
+                        <input type="password" id="pass" name="password" />
+                    </div>
+
+                    <input className="botao" type="submit" value="Entrar" />
+                </form>
+            </div>
+
+            <div id="cad-div">
+                <input id="cadastrar" className="botao" type="button" value="Cadastrar" />
+            </div>
+        </>
+    );
 }
