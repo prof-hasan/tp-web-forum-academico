fe204b111d3f26b7789aaccc7ab2436ee2119f70
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-08-06 19:10:08 -0300

    Ajustes para criar a tela de login

--- src/frontend-forum-academico/src/app/layout.tsx
+++ src/frontend-forum-academico/src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="imgs/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Rosario" rel="stylesheet" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
