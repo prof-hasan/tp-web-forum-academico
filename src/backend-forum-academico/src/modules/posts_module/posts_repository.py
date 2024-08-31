98eec13eb27a86c2a2df59f81f566bfb6c43afe6
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-08-31 20:40:20 -0300

    Usando generics para compartilhar a logica de repositorio

--- src/backend-forum-academico/src/modules/posts_module/posts_repository.py
+++ src/backend-forum-academico/src/modules/posts_module/posts_repository.py
@@ -1,6 +1,10 @@
 from ...repository import BaseRepository
 from ...database import DatabaseConstants
+from ...models import PostModel
+from typing import TypeVar
 
-class PostsRepository(BaseRepository):
+T = TypeVar('T', bound=PostModel)
+
+class PostsRepository(BaseRepository[T]):
     def __init__(self):
-        super().__init__(DatabaseConstants.POST_COLLECTION)
+        super().__init__(DatabaseConstants.POST_COLLECTION, PostModel)
