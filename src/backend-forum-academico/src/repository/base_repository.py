ecc765edb6d07036296e4c873eae95581d66511e
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-08-31 17:01:21 -0300

    Adicionado soft_delete

--- src/backend-forum-academico/src/repository/base_repository.py
+++ src/backend-forum-academico/src/repository/base_repository.py
@@ -1,7 +1,7 @@
 from bson import ObjectId
 from ..database import databseConnection
 from ..models import CustomBaseModel
-
+from datetime import datetime
 
 class BaseRepository:
     def __init__(self, collection_name: str):
@@ -12,14 +12,17 @@ class BaseRepository:
         return self.db[self.collection_name]
 
     async def find(self, filter: dict = {}):
+        filter["deleted_at"] = None
         return await self.__get_collection().find(filter).to_list(None)
     
     async def find_elements_paginated(self, filter: dict = {}, page:int = 1):
+        filter["deleted_at"] = None
         page_size = 2
         skip = (page - 1) * page_size
         return await self.__get_collection().find(filter).sort("created_at", -1).skip(skip).limit(page_size).to_list(None)
     
     async def find_one(self, filter: dict = {}):
+        filter["deleted_at"] = None
         return await self.__get_collection().find_one(filter)
     
     async def save(self, document: CustomBaseModel):
@@ -33,8 +36,9 @@ class BaseRepository:
             document.id = (await collection.insert_one(document.to_mongo_dict())).inserted_id
         return document
     
-    async def delete(self, document: CustomBaseModel):
-        await self.__get_collection().delete_one({"id": document.id})
+    async def soft_delete(self, document: CustomBaseModel):
+        document.deleted_at = datetime.now()
+        await self.save(document)
 
     async def delete_all(self):
         await self.__get_collection().delete_many({})
