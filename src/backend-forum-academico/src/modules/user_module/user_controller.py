545a4a19fb7674e852dd4e2fb739aaca2214bcc8
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-08-31 15:20:10 -0300

    Adicionando modelo na response para documentacao no swagger

--- src/backend-forum-academico/src/modules/user_module/user_controller.py
+++ src/backend-forum-academico/src/modules/user_module/user_controller.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
 from .user_repository import UserRepository
 from .user_domain import UserDomain
+from ...models import UserModel
 
 userRepository = UserRepository()
 user_domain = UserDomain(userRepository)
 
 user_router = APIRouter()
 
-@user_router.get("/users")
+@user_router.get("/users", response_model=list[UserModel])
 async def get_users():
     users = await user_domain.get_all_users()
     return [user.to_response_dict() for user in users]
