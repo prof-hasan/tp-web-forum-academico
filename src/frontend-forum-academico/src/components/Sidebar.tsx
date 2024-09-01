efa4bff1324a4d7edc7286c5795ea9515503384d
fsluizvictor <fs.luizvictor@gmail.com>
2024-09-01 18:43:48 -0300

    wip

--- src/frontend-forum-academico/src/components/Sidebar.tsx
+++ src/frontend-forum-academico/src/components/Sidebar.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import './sidebarStyles.css';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 const Sidebar: React.FC = () => {
   
   const router = useRouter();
+  const currentRoute = usePathname(); // Obtém a rota atual
+  console.log(currentRoute)
 
   const handleNavigation = (path: string) => {
     router.push(path);
@@ -17,20 +19,20 @@ const Sidebar: React.FC = () => {
       <img src="/logo.svg" alt="logo" className='logo' />
       <div className="icons">
         <button 
-          className="icon" 
-          onClick={() => handleNavigation('/latestPosts')}
+          className={`icon${currentRoute === '/cloud' ? '.active' : ''}`} 
+          onClick={() => handleNavigation('/cloud')}
         >
           ☁️
         </button>
         <button 
-          className="icon" 
-          onClick={() => handleNavigation('/myPosts')}
+          className={`icon${currentRoute === '/files' ? '.active' : ''}`} 
+          onClick={() => handleNavigation('/files')}
         >
           📁
         </button>
         <button 
-          className="icon" 
-          onClick={() => handleNavigation('/userProfile')}
+          className={`icon${currentRoute === '/profile' ? '.active' : ''}`} 
+          onClick={() => handleNavigation('/profile')}
         >
           👤
         </button>
