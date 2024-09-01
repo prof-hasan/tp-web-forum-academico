0ec1d5c63d931182cc37bff0a3945603240c987b
fsluizvictor <fs.luizvictor@gmail.com>
2024-09-01 18:38:20 -0300

    feat : add routes in sidebar

--- src/frontend-forum-academico/src/app/page.tsx
+++ src/frontend-forum-academico/src/app/page.tsx
@@ -1,10 +1,12 @@
-'use client'
+//'use client'
+
+import React from "react";
 import styles from "./page.module.css";
-import { useRouter } from "next/navigation";
+//import { useRouter } from 'next/router';
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/login");
+  //const router = useRouter();
+  //router.push("/login");
 
   return (
     <main className={styles.main}>
