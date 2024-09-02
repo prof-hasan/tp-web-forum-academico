35a5d55442ecdde7b3a52756717ace656ed4698e
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-09-02 08:17:15 -0300

    Update page.tsx

--- src/frontend-forum-academico/src/app/page.tsx
+++ src/frontend-forum-academico/src/app/page.tsx
@@ -1,12 +1,15 @@
-//'use client'
+'use client'
 
-import React from "react";
+import React, { use, useEffect } from "react";
 import styles from "./page.module.css";
-//import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  //const router = useRouter();
-  //router.push("/login");
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.push("/login");
+  }, []);
 
   return (
     <main className={styles.main}>
