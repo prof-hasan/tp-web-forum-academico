bc1384633e58db651b1c87e4e3fb9b8a36eaf498
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-09-03 23:28:54 -0300

    Criado pasta helpers

--- src/frontend-forum-academico/src/components/Post.tsx
+++ src/frontend-forum-academico/src/components/Post.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import './postStyles.css';
 import { PostProps } from '../commom/interfaces/postProps';
+import { formatDateTime } from '@/commom/helper/date';
 
 const Post: React.FC<PostProps> = ({ author, date, content, likes, comments })=>{
-  function formatDateTime(dateTime: string): string {
-    const date = new Date(dateTime);
-
-    const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: 'long', 
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false 
-    };
-
-    const formattedDate = date.toLocaleDateString('pt-BR', options);
-
-    return `${formattedDate}`;
-}
 
   return (
     <div className="post">
