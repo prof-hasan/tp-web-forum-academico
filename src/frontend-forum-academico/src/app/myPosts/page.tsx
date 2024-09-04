239d3f427b749a957639ad0470edf04fed1aecc4
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-09-03 22:33:32 -0300

    Busca dos posts do usuario para a pagina myPosts

--- src/frontend-forum-academico/src/app/myPosts/page.tsx
+++ src/frontend-forum-academico/src/app/myPosts/page.tsx
@@ -1,56 +1,31 @@
-// MyPosts.tsx
+'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../../components/Sidebar';
 import Post from '../../components/Post';
-import { PostProps } from '../../commom/interfaces/postProps';
 import './styles.css';
+import { usePosts } from '@/context/PostContext';
 
 const MyPosts: React.FC = () => {
-  const posts: PostProps[] = [
-    {
-      author: 'Zalter',
-      date: '04 feb 2024',
-      content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-      likes: 20,
-      comments: 17,
-    },
-    {
-      author: 'Zalter',
-      date: '04 feb 2024',
-      content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-      likes: 20,
-      comments: 17,
-    },
-    {
-      author: 'Zalter',
-      date: '04 feb 2024',
-      content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-      likes: 20,
-      comments: 17,
-    },
-    {
-      author: 'Zalter',
-      date: '04 feb 2024',
-      content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-      likes: 20,
-      comments: 17,
-    },
-  ];
+  const {getMyPosts, myPosts} = usePosts();
+
+  useEffect(() => {
+    getMyPosts();
+  }, [getMyPosts]);
 
   return (
     <div className="container">
       <Sidebar />
       <main className="main">
         <h1>Meus posts</h1>
-        {posts.map((post, index) => (
+        {myPosts.map((post, index) => (
           <Post
             key={index}
-            author={post.author}
-            date={post.date}
-            content={post.content}
-            likes={post.likes}
-            comments={post.comments}
+            author={`${post.user_name} ${post.title}`}
+            date={post.created_at}
+            content={post.text}
+            likes={post.likes.length}
+            comments={post.saveds.length}
           />
         ))}
       </main>
--- src/frontend-forum-academico/src/context/PostContext.tsx
+++ src/frontend-forum-academico/src/context/PostContext.tsx
@@ -1,14 +1,17 @@
 'use client'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode, useCallback } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ApiPost } from "@/commom/interfaces/postProps";
+import { headers } from 'next/headers';
 
 interface PostContextType {
   getPosts: () => void;
+  getMyPosts: () => void;
   posts: ApiPost[];
+  myPosts: ApiPost[];
 }
 
 const PostContext = createContext<PostContextType | undefined>(undefined);
@@ -17,23 +20,42 @@ export const PostProvider = ({ children }: { children: ReactNode }) => {
     const router = useRouter();
     const [page, setPage] = useState(1);
     const [posts, setPosts] = useState<[]>([]);
+    const [myPosts, setMyPosts] = useState<[]>([]);
 
-
-    const getPosts = async () => {
+    const getPosts = useCallback(async () => {
         try {
-            const response: any = await axios.get(`http://localhost:8000/posts/${page}`);
+            const token = localStorage.getItem('acessToken');
+            const response: any = await axios.get(`http://localhost:8000/posts/${page}`,{headers:{
+                "Authorization": `Bearer ${token}`,
+            }});
             setPosts(response.data);
         } catch (error: any) {
             if (error.response && error.response.status === 401) {
                 toast.error("Credenciais inválidas!"); 
             } else {
-                toast.error("Ocorreu um erro ao tentar fazer login. Por favor, tente novamente."); 
+                toast.error("Ocorreu um erro ao tentar carregar os posts. Por favor, tente novamente."); 
             }
         }
-    };
+    }, []);
+
+    const getMyPosts = useCallback(async () => {
+        try {
+            const token = localStorage.getItem('acessToken');
+            const response: any = await axios.get(`http://localhost:8000/user_posts`,{headers:{
+                "Authorization": `Bearer ${token}`,
+            }});
+            setMyPosts(response.data);
+        } catch (error: any) {
+            if (error.response && error.response.status === 401) {
+                toast.error("Credenciais inválidas!"); 
+            } else {
+                toast.error("Ocorreu um erro ao carregar os seus posts. Tente novamente."); 
+            }
+        }
+    }, []);
 
     return (
-        <PostContext.Provider value={{ getPosts, posts }}>
+        <PostContext.Provider value={{ getPosts, getMyPosts, posts, myPosts }}>
             {children}
         </PostContext.Provider>
     );
